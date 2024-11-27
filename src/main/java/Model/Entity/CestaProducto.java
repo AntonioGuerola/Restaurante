52f1fe8c6481402233037753da5f0c1191b1bc06
Guerola <antonioguerolacrespo@gmail.com>
2024-11-27 20:04:35 +0100

    Some entities arrangements and DAOS advances

--- src/main/java/Model/Entity/CestaProducto.java
+++ src/main/java/Model/Entity/CestaProducto.java
@@ -1,22 +1,66 @@
 package Model.Entity;
 
-import java.util.HashMap;
+import Model.Entity.*;
+import java.util.Objects;
 
 public class CestaProducto {
     private Cesta cesta;
-    private HashMap<Integer, Producto> producto;
+    private Producto producto;
     private int cantidad;
-    private String destino;
 
-    public CestaProducto(Cesta cesta, HashMap<Integer, Producto> producto, int cantidad, String destino) {
+    public CestaProducto(Cesta cesta, Producto producto, int cantidad) {
         this.cesta = cesta;
         this.producto = producto;
         this.cantidad = cantidad;
-        this.destino = destino;
     }
 
     public CestaProducto() {
         cantidad = 0;
-        this.producto = new HashMap<>();
+    }
+
+    public Cesta getCesta() {
+        return cesta;
+    }
+
+    public void setCesta(Cesta cesta) {
+        this.cesta = cesta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        CestaProducto that = (CestaProducto) object;
+        return Objects.equals(cesta, that.cesta);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cesta);
+    }
+
+    @Override
+    public String toString() {
+        return "CestaProducto{" +
+                "cesta=" + cesta +
+                ", producto=" + producto +
+                ", cantidad=" + cantidad +
+                '}';
     }
 }
